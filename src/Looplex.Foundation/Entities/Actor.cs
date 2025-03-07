b4e33353763364a9be2e9340d46bbc3a1266d18a
Fabio Zendhi Nagao <nagaozen@hotmail.com>
2025-03-07 00:33:30 -0300

    docs(entities): review and add useful comments

--- src/Looplex.Foundation/Entities/Actor.cs
+++ src/Looplex.Foundation/Entities/Actor.cs
@@ -1,35 +1,37 @@
 using System;
 
-namespace Looplex.Foundation.Entities;
-
-public abstract class Actor
-{
-  #region Reflectivity
-
-  // ReSharper disable once PublicConstructorInAbstractClass
-  public Actor() { }
-  
-  #endregion
-
-  #region Observability
-
-  private event Action<string, object> ActorEvent;
-
-  protected void FireEvent(string eventName, object data = null)
+namespace Looplex.Foundation.Entities {
+  public abstract class Actor
   {
-    Action<string, object> handler = ActorEvent; // Prevent potential race conditions on MTA environments
-    ActorEvent?.Invoke(eventName, data); // Notify subscribers
-  }
+    #region Reflectivity
+    /* NOTE: Normally, since abstract classes can’t be instantiated directly,
+    ReSharper might flag a public constructor as unnecessary or potentially
+    indicative of a design flaw. However, there are cases—like using reflection
+    or providing a clear initialization path for derived classes—where a public
+    constructor is intentional. By adding this comment, you're telling ReSharper:
+    "I know what I'm doing here; don't warn me about this one time."
+    */
+    // ReSharper disable once PublicConstructorInAbstractClass
+    public Actor() { }
+    #endregion
 
-  public void AddEventListener(Action<string, object> handler)
-  {
-    ActorEvent += handler ?? throw new ArgumentNullException(nameof(handler));
-  }
+    #region Observability
+    private event Action<string, object> ActorEvent;
+    protected void FireEvent(string eventName, object data = null)
+    {
+      Action<string, object> handler = ActorEvent; // Prevent potential race conditions on MTA environments
+      ActorEvent?.Invoke(eventName, data); // Notify subscribers
+    }
 
-  public void RemoveEventListener(Action<string, object> handler)
-  {
-    ActorEvent -= handler ?? throw new ArgumentNullException(nameof(handler));
-  }
+    public void AddEventListener(Action<string, object> handler)
+    {
+      ActorEvent += handler ?? throw new ArgumentNullException(nameof(handler));
+    }
 
-  #endregion
+    public void RemoveEventListener(Action<string, object> handler)
+    {
+      ActorEvent -= handler ?? throw new ArgumentNullException(nameof(handler));
+    }
+    #endregion
+  }
 }
--- src/Looplex.Foundation/Entities/Service.cs
+++ src/Looplex.Foundation/Entities/Service.cs
@@ -1,33 +1,36 @@
+using System;
 using System.Collections.Generic;
 
 using Looplex.OpenForExtension.Abstractions.Contexts;
 using Looplex.OpenForExtension.Abstractions.Plugins;
 using Looplex.OpenForExtension.Contexts;
 
-namespace Looplex.Foundation.Entities;
-
-public abstract class Service
-{
-  #region Reflectivity
-
-  // ReSharper disable once PublicConstructorInAbstractClass
-  public Service() { }
-
-  #endregion
-
-  protected Service(IList<IPlugin> plugins)
+namespace Looplex.Foundation.Entities {
+  public abstract class Service
   {
-    Plugins = plugins;
+    #region Reflectivity
+    /* NOTE: Normally, since abstract classes can’t be instantiated directly,
+    ReSharper might flag a public constructor as unnecessary or potentially
+    indicative of a design flaw. However, there are cases—like using reflection
+    or providing a clear initialization path for derived classes—where a public
+    constructor is intentional. By adding this comment, you're telling ReSharper:
+    "I know what I'm doing here; don't warn me about this one time."
+    */
+    // ReSharper disable once PublicConstructorInAbstractClass
+    public Service() { }
+    #endregion
+
+    #region Micro-Kernel
+    protected Service(IList<IPlugin> plugins)
+    {
+      Plugins = plugins;
+    }
+    protected IList<IPlugin> Plugins { get; set; } = new List<IPlugin>();
+
+    public virtual IContext NewContext()
+    {
+      return DefaultContext.New(Plugins);
+    }
+    #endregion
   }
-
-  #region Micro-Kernel
-
-  protected IList<IPlugin> Plugins { get; set; } = new List<IPlugin>();
-
-  public virtual IContext NewContext()
-  {
-    return DefaultContext.New(Plugins);
-  }
-
-  #endregion
 }
