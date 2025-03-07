fe094d1da17177c950fc310142f389be16285287
Fabio Zendhi Nagao <nagaozen@hotmail.com>
2025-03-07 00:57:01 -0300

    refactor(entities): every service "is-a" actor

--- src/Looplex.Foundation/Entities/Service.cs
+++ src/Looplex.Foundation/Entities/Service.cs
@@ -6,20 +6,8 @@ using Looplex.OpenForExtension.Abstractions.Plugins;
 using Looplex.OpenForExtension.Contexts;
 
 namespace Looplex.Foundation.Entities {
-  public abstract class Service
+  public abstract class Service : Actor
   {
-    #region Reflectivity
-    /* NOTE: Normally, since abstract classes can’t be instantiated directly,
-    ReSharper might flag a public constructor as unnecessary or potentially
-    indicative of a design flaw. However, there are cases—like using reflection
-    or providing a clear initialization path for derived classes—where a public
-    constructor is intentional. By adding this comment, you're telling ReSharper:
-    "I know what I'm doing here; don't warn me about this one time."
-    */
-    // ReSharper disable once PublicConstructorInAbstractClass
-    public Service() { }
-    #endregion
-
     #region Micro-Kernel
     protected Service(IList<IPlugin> plugins)
     {
