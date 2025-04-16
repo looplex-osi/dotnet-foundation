f99ce7a0997395f3efa082bb6b472cfe8a98b9b7
Guilherme Câmara <guilherme.camara@outlook.com.br>
2025-04-16 15:33:38 -0300

    refactor(scimv2): removed project antlr

--- src/Looplex.Foundation/Helpers/Strings.cs
+++ src/Looplex.Foundation/Helpers/Strings.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Text;
 
+using Antlr4.Runtime;
+
+using Looplex.Foundation.SCIMv2.Antlr;
+using Looplex.Foundation.SCIMv2.Entities;
+
 namespace Looplex.Foundation.Helpers;
 
 public static class Strings
@@ -9,5 +14,28 @@ public static class Strings
   {
     byte[] base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
     return Encoding.UTF8.GetString(base64EncodedBytes);
+  }
+  
+  /// <summary>
+  /// Converts a SCIMv2 defined filters query param into a SQL predicate 
+  /// </summary>
+  /// <param name="filters"></param>
+  /// <returns></returns>
+  private static string? ToSqlPredicate(this string? filters)
+  {
+    string? result = null;
+
+    if (!string.IsNullOrEmpty(filters))
+    {
+      var inputStream = new AntlrInputStream(filters);
+      var lexer = new ScimFilterLexer(inputStream);
+      var tokens = new CommonTokenStream(lexer);
+      var parser = new ScimFilterParser(tokens);
+      var tree = parser.parse();
+      var visitor = new SCIMv2ToSQLVisitor();
+      result = visitor.Visit(tree);
+    }
+
+    return result;
   }
 }
