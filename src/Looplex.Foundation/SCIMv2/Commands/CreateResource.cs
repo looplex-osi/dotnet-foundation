fa38e5f60873aa599bf1dcb7256b28ab5b1c1dcb
Fabio Pereira dos Santos <fabio.santos@looplex.com.br>
2025-07-04 09:29:04 -0300

    chore(scimv2): change object? to IDbTransaction?

--- src/Looplex.Foundation/SCIMv2/Commands/CreateResource.cs
+++ src/Looplex.Foundation/SCIMv2/Commands/CreateResource.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 
 using Looplex.Foundation.SCIMv2.Entities;
 
@@ -6,9 +7,9 @@ using MediatR;
 
 namespace Looplex.Foundation.SCIMv2.Commands;
 
-public class CreateResource<T>(T resource, object? dbTransaction = null) : IRequest<Guid> where T : Resource
+public class CreateResource<T>(T resource, IDbTransaction? dbTransaction = null) : IRequest<Guid> where T : Resource
 {
   public T Resource { get; } = resource ?? throw new ArgumentNullException(nameof(resource));
 
-  public object? DbTransaction { get; set; } = dbTransaction;
+  public IDbTransaction? DbTransaction { get; set; } = dbTransaction;
 }
