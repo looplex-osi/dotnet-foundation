282c776e88988d5ba0fc0b676832978f5c9a4a1b
jjfinoquetof <joao.filho@looplex.com.br>
2025-10-08 10:57:02 -0300

    refactor: rename Base64Decode to Uft8FromBase64
    - Rename method from Base64Decode to Uft8FromBase64 for better clarity
    - Method functionality remains the same (Base64 decode + UTF8 string conversion)

--- src/Looplex.SCIMv2/Helpers/Strings.cs
+++ src/Looplex.SCIMv2/Helpers/Strings.cs
@@ -11,7 +11,7 @@ namespace Looplex.SCIMv2.Helpers;
 
 public static class Strings
 {
-  public static string Base64Decode(string base64EncodedData)
+  public static string Uft8FromBase64(string base64EncodedData)
   {
     byte[] base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
     return Encoding.UTF8.GetString(base64EncodedBytes);
