95889a07256b41cdb28ae37826b2b9df9db9763f
Fabio Pereira dos Santos <fabio.santos@looplex.com.br>
2026-05-27 18:24:35 -0300

    feat(scimv2): implement POST /.search endpoint
    [AB#34104](https://dev.azure.com/looplex/projetos/_workitems/edit/34104)

---
+++ src/Looplex.Foundation/SCIMv2/Entities/SCIMv2Search.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace Looplex.Foundation.SCIMv2.Entities;
+
+public class SCIMv2Search
+{
+  [JsonProperty("schemas")]
+  public string[] Schemas { get; set; } = [];
+
+  [JsonProperty("filter")]
+  public string? Filter { get; set; } = null;
+
+  [JsonProperty("sortBy")]
+  public string? SortBy { get; set; } = null;
+
+  [JsonProperty("sortOrder")]
+  public string? SortOrder { get; set; } = null;
+
+  [JsonProperty("startIndex")]
+  public int StartIndex { get; set; } = 1;
+
+  [JsonProperty("count")]
+  public int Count { get; set; } = 10;
+}
